52dea069c59318a2928d69e6a31dcbb544687914
chibeya <109008286+chibeya@users.noreply.github.com>
2022-07-22 22:26:55 +0900

    Add files via upload

---
+++ HomeWork_0717_1.java
@@ -0,0 +1,10 @@
+public class HomeWork_0717_1 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 9; i++) {
+			for (int n = 2; n <= 9; n++) {
+				System.out.print(n + "*" + i + "=" + (i * n) + "\t");
+			}
+			System.out.println();
+		}
+	}
+}
